0e1849403dade0b5fe5d3639c85d1fae14d30205
Brady Trainor <mail@bradyt.com>
2018-12-11 17:53:00 -0800

    Still errors?

--- test/dart-mode-test.el
+++ test/dart-mode-test.el
@@ -9,8 +9,8 @@
     (dart-add-analysis-root-for-file file)
     (let ((proc (dart--analysis-server-process
 		 dart--analysis-server)))
-      (accept-process-output proc)
-      (accept-process-output proc)
+      ;; (accept-process-output proc)
+      ;; (accept-process-output proc)
       (accept-process-output proc 8))))
 
 ;;; dart-mode-test.el ends here
